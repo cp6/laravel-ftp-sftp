1d91f42d737f4574f4cd179102b54167237eb38f
cp6 <cp6@users.noreply.github.com>
2024-05-24 00:00:05 +1000

    Added connections fillable

--- app/Models/Connection.php
+++ app/Models/Connection.php
@@ -8,4 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Connection extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['is_sftp', 'host', 'username', 'password', 'port', 'timeout', 'log_actions', 'key'];
+
 }
