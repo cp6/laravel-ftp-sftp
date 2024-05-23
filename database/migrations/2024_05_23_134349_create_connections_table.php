37b3f0a6de8f8e7cdccaa312e8118c44be8eccf4
cp6 <cp6@users.noreply.github.com>
2024-05-23 23:59:33 +1000

    Added connections columns

--- database/migrations/2024_05_23_134349_create_connections_table.php
+++ database/migrations/2024_05_23_134349_create_connections_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,7 +12,18 @@ return new class extends Migration
     {
         Schema::create('connections', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->boolean('is_sftp');
+            $table->string('host');
+            $table->string('username');
+            $table->string('password')->nullable();
+            $table->integer('port');
+            $table->integer('timeout')->default(6);
+            $table->boolean('log_actions')->default(1);
+            $table->text('key')->default(null)->nullable();
             $table->timestamps();
+            $table->unique(['user_id', 'host', 'is_sftp', 'username']);
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
