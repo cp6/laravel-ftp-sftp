fb3d399701c43950233cd1a12fabfdec54595f5a
cp6 <cp6@users.noreply.github.com>
2024-05-23 23:49:59 +1000

    Added api_token to users

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->char('api_token', 64)->unique();
             $table->timestamps();
         });
 
