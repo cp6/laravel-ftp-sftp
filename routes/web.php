9c05d4aebf2f828d297a20323a0f07bb01d451b7
cp6 <cp6@users.noreply.github.com>
2024-05-24 00:08:20 +1000

    Added connection routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ConnectionController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -12,9 +12,12 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/connections', [ConnectionController::class, 'index'])->name('connection.index');
+    Route::get('/connections/add', [ConnectionController::class, 'create'])->name('connection.create');
+    Route::post('/connections', [ConnectionController::class, 'store'])->name('connection.store');
+    Route::get('/connections/{connection}/edit', [ConnectionController::class, 'edit'])->name('connection.edit');
+    Route::patch('/connections/{connection}', [ConnectionController::class, 'update'])->name('connection.update');
+    Route::delete('/connections/{connection}', [ConnectionController::class, 'destroy'])->name('connection.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
