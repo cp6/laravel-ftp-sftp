4483393f0b964bb5c36fd14cfc360ef77dab6ba8
cp6 <cp6@users.noreply.github.com>
2024-05-27 21:54:47 +1000

    Fixed log_actions being null when not toggled

--- resources/views/connection/create.blade.php
+++ resources/views/connection/create.blade.php
@@ -51,6 +51,7 @@
                         <div class="mb-4 mt-8 md:col-span-6 col-span-12">
                             <div class="flex items-center mb-4">
                                 <fieldset>
+                                    <input type="hidden" value="0" name="log_actions">
                                     <input checked id="log_actions" type="checkbox" value=""
                                            class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
                                     <label for="log_actions"
