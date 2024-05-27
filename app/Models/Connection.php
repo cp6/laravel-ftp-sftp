04d29309efcc2d023d832640306fd42f736dda67
cp6 <cp6@users.noreply.github.com>
2024-05-27 21:53:49 +1000

    Added makeSftpConnectionPassword() to Connection.php

--- app/Models/Connection.php
+++ app/Models/Connection.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Str;
+use phpseclib3\Net\SFTP;
 
 class Connection extends Model
 {
@@ -19,6 +20,19 @@ class Connection extends Model
             $connection->sid = Str::random(12);
             $connection->user_id = Auth::id();
         });
+    }
+
+    public static function makeSftpConnectionPassword(string $host, int $port, string $user, string $password, int $timeout = 8): ?SFTP
+    {
+        $sftp = new SFTP($host, $port, $timeout);
+
+        try {
+            $sftp->login($user, $password);
+        } catch (\Exception $exception) {
+            return null;
+        }
+
+        return $sftp;
     }
 
 
