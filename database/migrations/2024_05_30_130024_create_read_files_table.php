d9b924876bf126069d5f8d0dd8d97177abb8a5e4
cp6 <cp6@users.noreply.github.com>
2024-05-31 00:04:22 +1000

    Updated 2024_05_30_130024_create_read_files_table.php

--- database/migrations/2024_05_30_130024_create_read_files_table.php
+++ database/migrations/2024_05_30_130024_create_read_files_table.php
@@ -13,8 +13,9 @@ return new class extends Migration
     {
         Schema::create('read_files', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('file_id');
+            $table->unsignedBigInteger('file_id')->unique();
             $table->integer('last_line_read')->nullable()->default(null);
+            $table->integer('total_lines')->nullable()->default(null);
             $table->timestamps();
             $table->foreign('file_id')->references('id')->on('users')->onDelete('cascade');
         });
