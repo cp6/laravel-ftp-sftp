5a75e4b8b9e0ec41c51ed3cf8fd29f43108a1987
cp6 <cp6@users.noreply.github.com>
2024-05-31 00:04:09 +1000

    Updated 2024_05_23_134509_create_files_table.php

--- database/migrations/2024_05_23_134509_create_files_table.php
+++ database/migrations/2024_05_23_134509_create_files_table.php
@@ -13,7 +13,19 @@ return new class extends Migration
     {
         Schema::create('files', function (Blueprint $table) {
             $table->id();
+            $table->char('sid', 12)->unique();//String id
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('connection_id');
+            $table->integer('size_kb')->nullable()->default(null);
+            $table->string('ext')->nullable()->default(null);
+            $table->string('saved_to')->nullable()->default(null);
+            $table->string('saved_as')->nullable()->default(null);
+            $table->string('original_dir')->nullable()->default(null);
+            $table->string('original_name')->nullable()->default(null);
             $table->timestamps();
+            $table->unique(['connection_id', 'original_dir', 'original_name']);
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('connection_id')->references('id')->on('connections')->onDelete('cascade');
         });
     }
 
