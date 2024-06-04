79d7a35cd545e9d40bedae47822357d233dda04d
cp6 <cp6@users.noreply.github.com>
2024-06-04 15:32:57 +1000

    Removed use Illuminate\Http\Response; from File

--- app/Models/File.php
+++ app/Models/File.php
@@ -4,7 +4,6 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Crypt;
 use Illuminate\Support\Facades\Log;
